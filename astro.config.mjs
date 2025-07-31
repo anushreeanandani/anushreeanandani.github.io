6a600f7c8b9fed38689dfbf19cc1c1c6512473fd
Anushree Anandani <aa11093@nyu.edu>
2025-07-31 11:24:36 -0400

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://anushreeanandani.github.io',
+  base: '/',
+  output:'static',
   integrations: [icon()],
   prefetch: true,
 });
